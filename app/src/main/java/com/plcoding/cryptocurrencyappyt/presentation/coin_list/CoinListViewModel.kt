419853f7e0c05aa8fe7cf92ee4bb3cfd6273e224
Carlos Padilha <carlospadilhadev@gmail.com>
2023-06-06 16:23:20 -0300

    Implementação das Views Models

---
+++ app/src/main/java/com/plcoding/cryptocurrencyappyt/presentation/coin_list/CoinListViewModel.kt
@@ -0,0 +1,43 @@
+package com.plcoding.cryptocurrencyappyt.presentation.coin_list
+
+import androidx.compose.runtime.State
+import androidx.compose.runtime.mutableStateOf
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.plcoding.cryptocurrencyappyt.common.Resource
+import com.plcoding.cryptocurrencyappyt.presentation.use_case.get_coins_list.GetCoinsListUseCases
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.launchIn
+import kotlinx.coroutines.flow.onEach
+import javax.inject.Inject
+
+@HiltViewModel
+class CoinListViewModel @Inject constructor(
+    private val getCoinsListUseCase: GetCoinsListUseCases
+) : ViewModel() {
+
+    private val _state = mutableStateOf(CoinListState())
+    val state: State<CoinListState> = _state
+
+    init {
+        getCoins()
+    }
+
+    private fun getCoins() {
+        getCoinsListUseCase().onEach { result ->
+            when (result) {
+                is Resource.Success -> {
+                    _state.value = CoinListState(coins = result.data ?: emptyList())
+                }
+                is Resource.Error -> {
+                    _state.value = CoinListState(
+                        error = result.message ?: "Um erro inesperado ocorreu!"
+                    )
+                }
+                is Resource.Loading -> {
+                    _state.value = CoinListState(isLoading = true)
+                }
+            }
+        }.launchIn(viewModelScope)
+    }
+}
